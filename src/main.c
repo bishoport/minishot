487f725a7b1c3fa224b29e820f5fb83d0c1d5eef
bishoport <bishoport@gmail.com>
2022-11-23 14:12:31 +0100

    mas cosas

--- src/main.c
+++ src/main.c
@@ -20,8 +20,31 @@ enum{
 };
 
 
+void drawPlayer(){
+   u8 *pvmen = cpct_getScreenPtr(CPCT_VMEM_START,PLAYER_X,PLAYER_Y);
+   cpct_drawSprite(sp_player_ship,pvmen,SP_PLAYER_SHIP_W,SP_PLAYER_SHIP_H);
+}
+
+void drawEnemy(){
+   u8 *pvmen = cpct_getScreenPtr(CPCT_VMEM_START,ENEMY_X,ENEMY_Y);
+   cpct_drawSprite(sp_enemy_ship,pvmen,SP_ENEMY_SHIP_W,SP_ENEMY_SHIP_H);
+}
+
+void drawShoot(){
+   u8 *pvmen = cpct_getScreenPtr(CPCT_VMEM_START,SHOT_X,SHOT_Y);
+   cpct_drawSprite(sp_shoot,pvmen,SP_SHOOT_W,SP_SHOOT_H);
+}
+
 //En el antiguo C no se pueden
 void main(void) {
+
+   //DEFINICION DE VARIABLES
+   i8 temperatura             = -8; //Para ir con signo -128 -> 127
+   u8 enemy_x                 = 80; //u8 es notacion de CPCTelera, en C es unsigned int
+   u32 potencia_arma_1        = 25910;//unsigned long en C
+   f32 porcentaje             = 0.43; // 10 E -27, 5*10^27, +6*2⁻15 Usa la notacion cientifica para almacenar decimales tochos //float
+   const char *mensaje_win[]  = "Has ganado!"; //Array de caracteres de solo lectura-> mensaje_win es la direccion de memoria donde empieza
+
 
    cpct_disableFirmware(); //Dejamos de ejecutar el Firmware default del Amstrad (todo bajo nuestro control)
    cpct_setVideoMode(0); //Ponemos el Modo de video 0 (4 colores)
@@ -30,21 +53,10 @@ void main(void) {
 
 //En el antiguo C no se pueden crear variables a mitad de bloque de funcion. Pero se pueden definir subBloques
 //Los bloques son necesarios para que el compidador tenga en cuenta las vriables al principio, son ambitos o scopes
-{
-   u8 *pvmen = cpct_getScreenPtr(CPCT_VMEM_START,PLAYER_X,PLAYER_Y);
-   cpct_drawSprite(sp_player_ship,pvmen,SP_PLAYER_SHIP_W,SP_PLAYER_SHIP_H);
-}
 
-{
-   u8 *pvmen = cpct_getScreenPtr(CPCT_VMEM_START,ENEMY_X,ENEMY_Y);
-   cpct_drawSprite(sp_enemy_ship,pvmen,SP_ENEMY_SHIP_W,SP_ENEMY_SHIP_H);
-}
-
-{
-   u8 *pvmen = cpct_getScreenPtr(CPCT_VMEM_START,SHOT_X,SHOT_Y);
-   cpct_drawSprite(sp_shoot,pvmen,SP_SHOOT_W,SP_SHOOT_H);
-}
-   
+   drawPlayer();
+   drawEnemy();
+   drawShoot();
    
    while (TRUE);
 }
